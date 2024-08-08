a32217dea72275d759784f54cbebf7fae3ffd396
Kevsin <20230002@ricaldone.edu.sv>
2024-08-07 20:34:51 -0500

    Cambios Kev 4

--- Hospitalbloom/app/src/main/java/PacientesHelpers/tbPacientes.kt
+++ Hospitalbloom/app/src/main/java/PacientesHelpers/tbPacientes.kt
@@ -1,14 +1,11 @@
 package PacientesHelpers
 
-data class tbPacientes(
-    var UUID_Paciente: String,
-    var Nombres: String,
-    var Apellidos: String,
-    var Edad: String,
-    var FechaNacimiento: String,
-    var Enfermedad: String,
-    var Medicamento: String,
-    var HoraAplicacion: String,
-    var Habitacion: String,
-    var Cama: String
+data class TbPacientes(
+    val UUID_Paciente : String,
+    val Nombres : String,
+    val Apellidos : String,
+    val Fecha_Nacimiento : String,
+    val Edad : String,
+    val Habitacion : String,
+    val Cama : String
 )
